e69032c67d427961ae841b51e189ccf46530fe1e
Greg Azevedo <gazevedo@umich.edu>
2014-10-20 12:51:34 -0400

    makes a watson call with good response

--- WatsonQuery/ViewController.m
+++ WatsonQuery/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "WPWatson.h"
 
 @interface ViewController ()
 
@@ -17,46 +18,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSDictionary *question = @{@"question": @[
-                                       @{@"questionText": @"What is sepsis?"},
-                                       @{@"items":@4}]};
-    NSURL *url = [NSURL URLWithString:@"https://watsonwdc01.ihost.com/instance/509/deepqa/v1/question"]; //instance #: 509
+    [[WPWatson sharedManager] askQuestion:@"What is sepsis?" completionHandler:^(NSError *connectionError) {
+        //
+    }];
     
-    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    sessionConfiguration.HTTPAdditionalHeaders = @{@"api-key": @"API_KEY"};
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     
-    NSError *error;
-    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:question options:NSJSONWritingPrettyPrinted error:&error];
-    request.HTTPMethod = @"POST";
-    NSURLSessionDataTask *postDataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) { }];
-    [postDataTask resume];
+//    NSDictionary *question = @{@"question": @[
+//                                       @{@"questionText": @"What is sepsis?"},
+//                                       @{@"items":@4}]};
+//    NSURL *url = [NSURL URLWithString:@"https://watson-wdc01.ihost.com/instance/509/deepqa/v1/question"]; //instance #: 509
+//    
+//    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    sessionConfiguration.HTTPAdditionalHeaders = @{@"api-key": @"API_KEY"};
+//    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+//    
+//    NSError *error;
+//    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:question options:NSJSONWritingPrettyPrinted error:&error];
+//    request.HTTPMethod = @"POST";
+//    NSURLSessionDataTask *postDataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) { }];
+//    [postDataTask resume];
+    
+    
+    
 }
                                           
-- (NSData *)httpBodyForDictionary:(NSDictionary *)dict
-{
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:question
-                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
-                                                         error:&error];
-    if (!jsonData) {
-        NSLog(@"Got an error: %@", error);
-    } else {
-        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    }
-}
-
-+(NSString*) getBase64AuthHeaderWithUsername:(NSString*) user andPassword:(NSString*) password
-{
-    NSString* base64Login = [NSString stringWithFormat:@"%@:%@", user, password];
-    
-    NSData *plainDataLogin = [base64Login dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *base64StringLogin = [NSString stringWithFormat:@"Basic %@", [plainDataLogin base64EncodedStringWithOptions:kNilOptions]];
-    
-    return base64StringLogin;
-}
-
+//- (NSData *)httpBodyForDictionary:(NSDictionary *)dict
+//{
+//    NSError *error;
+//    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:question
+//                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+//                                                         error:&error];
+//    if (!jsonData) {
+//        NSLog(@"Got an error: %@", error);
+//    } else {
+//        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+//    }
+//}
+//
+//+(NSString*) getBase64AuthHeaderWithUsername:(NSString*) user andPassword:(NSString*) password
+//{
+//    NSString* base64Login = [NSString stringWithFormat:@"%@:%@", user, password];
+//    
+//    NSData *plainDataLogin = [base64Login dataUsingEncoding:NSUTF8StringEncoding];
+//    NSString *base64StringLogin = [NSString stringWithFormat:@"Basic %@", [plainDataLogin base64EncodedStringWithOptions:kNilOptions]];
+//    
+//    return base64StringLogin;
+//}
+//
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
